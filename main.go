f53b519bbcca05d56a02dd7106f9efca37cdce57
David Peinado <dpeinado@lucentialab.es>
2018-05-20 19:28:03 +0200

    Add heroku specific port and metrics logger

--- main.go
+++ main.go
@@ -3,13 +3,21 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
+
+	_ "github.com/heroku/x/hmetrics/onload"
 )
 
 const assetsDir = "assets"
 
 func main() {
+	var port string
+	port = os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
 	http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir(assetsDir))))
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
 }
